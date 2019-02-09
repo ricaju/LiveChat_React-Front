e47338f022f1f229dd4d558408d1d5d3528fa741
ricaju <jurica.martinez@gmail.com>
2019-02-09 19:48:19 +0100

    rout

--- src/conteiners/App.js
+++ src/conteiners/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Router, Route } from 'react-router-dom';
 import Login from './Login';
 import Registration from './Registration';
 
 class App extends Component {
   render() {
     return(
-      <Login/>
+      <Registration/>
       )
     }
 }
