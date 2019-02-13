0afaa15a67ea645b21c6e24c0cefb446b0988c84
ivonaki <ivonakizlin0@gmail.com>
2019-02-13 19:03:21 +0100

    1.2.

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,24 +1,7 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap'; 
 import axios from 'axios';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import './Login.css';
-<<<<<<< HEAD
-import Logo from '../component/Logo/Logo.js';
-=======
->>>>>>> parent of a282da6... version 1.1
-=======
-import Logo from './Logo/Logo.js';
->>>>>>> parent of 8ffd2f5... ccccc
-=======
->>>>>>> parent of 6dff0b3... bbb
-=======
->>>>>>> parent of a282da6... version 1.1
-=======
->>>>>>> parent of 6dff0b3... bbb
 
 class Login extends Component {
 	constructor(props) {
@@ -38,20 +21,11 @@ class Login extends Component {
     axios.post('/getToken', {   //token
       email: this.state.username,
       password: this.state.password
-    }).then(res => sessionStorage.setItem('MP-jwt', res.data)); //MP = "moj prvi :)"
+    }).then(res => localStorage.setItem('MP-jwt', res.data)); //MP = "moj prvi :)"
 	};
 
-	render() {
+   render() {
     return (
-      <div className="Login">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of ca6b9ed... Revert "aaa"
-=======
->>>>>>> parent of ca6b9ed... Revert "aaa"
       <div className="col-md-10" id='bc-login'>
         <Form onSubmit={e => this.handleSubmit(e)}>
           <FormGroup>
@@ -63,58 +37,17 @@ class Login extends Component {
                 placeholder="Type your username"
                 value={this.state.username}
                 onChange = {e => this.handleChange(e)} />
-=======
-=======
->>>>>>> parent of a282da6... version 1.1
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="username" size="large">
-            <FormLabel>Username</FormLabel>
-            <FormControl
-              type="username"
-              onChange={e => this.handleChange(e)}
-            />
-<<<<<<< HEAD
->>>>>>> parent of a282da6... version 1.1
-=======
->>>>>>> parent of a282da6... version 1.1
           </FormGroup>
-          <FormGroup controlId="password" size="large">
-            <FormLabel>Password</FormLabel>
-            <FormControl
-              type="password"        //value?
-              onChange={e => this.handleChange(e)}
-            />
+          <FormGroup>
+            <Label className= 'white' htmlFor="password">Password</Label>
+              <Input 
+                type="password" 
+                name="password" 
+                id="password" 
+                placeholder="Type your password"
+                value={this.state.password}
+                onChange = {e => this.handleChange(e)} />
           </FormGroup>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> parent of 8ffd2f5... ccccc
-=======
->>>>>>> parent of 6dff0b3... bbb
-=======
->>>>>>> parent of a282da6... version 1.1
-=======
->>>>>>> parent of 6dff0b3... bbb
-          <Button
-            block
-            size="large"
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
           <Button 
             type="submit" 
             name="submit" 
@@ -122,28 +55,9 @@ class Login extends Component {
             color="primary"
             >Login </Button>
         </Form>
->>>>>>> parent of 71ac842... vracanje
->>>>>>> parent of 8ffd2f5... ccccc
-=======
->>>>>>> parent of 6dff0b3... bbb
-=======
->>>>>>> parent of ca6b9ed... Revert "aaa"
-      </div>
-=======
->>>>>>> parent of a282da6... version 1.1
-=======
->>>>>>> parent of a282da6... version 1.1
-=======
-=======
->>>>>>> parent of 6dff0b3... bbb
-      </div>
-=======
->>>>>>> parent of a282da6... version 1.1
->>>>>>> parent of ca6b9ed... Revert "aaa"
-      </div>
+      </div>      
     );
   }
 }
-
 
 export default Login;
