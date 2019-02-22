f76d056644cef5a20e6671b6ebcc99279261d99c
ricaju <jurica.martinez@gmail.com>
2019-02-22 11:43:35 +0100

    query working version Apollo

--- src/component/ChatBox.js
+++ src/component/ChatBox.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import gql  from 'graphql-tag';
 import { graphql } from 'react-apollo';
-import { Query } from 'react-apollo'
+import { Query } from 'react-apollo';
 
 /*const subscription = gql`
   subscription messageAdded($chatroomId: String!) {
@@ -12,37 +12,33 @@ import { Query } from 'react-apollo'
 `; */
 
 const messages = gql` 
-	query{messages(chatroomId:"1") {
-	  id
-	  text
+	{
+	messages(chatroomId:"1") {
+		id
+		text
 	}}
 `; 
  
 class ChatBox extends Component {
-	displayMessages() {
-		let data = this.props.data;
-		if(data.loading){
-			return(<div> Loading messages...</div>);
-		} else {
-			return data.messages.map( message => {
-				return( <li key={message.id}> {message.text}</li>);
-
-			})
-
-		} 
-	}
 	render() {
-		console.log(this.props)  //test
-	return (	
-		<div>     {/* set conteiner for whole chat box?*/}
-			<div>
-						{/* set conteiner for sender and messages?*/}
-				<h4>{this.displayMessages()} </h4>
-				<p>{/*{message.content}*/} blablab</p>
-			</div>
-		</div>	
+		return (
+			<Query query={messages}>
+				{({ loading, error, data }) => {
+			        if (loading) return <div>Loading...</div>
+			        if (error) return <div>Error!</div>
+    
+         		 
+         	return (
+				<div>     {/* set conteiner for whole chat box?*/}
+					<div>							{/* set conteiner for sender and messages?*/}
+					{data.messages.map( message =>  <li key={message.id}> {message.text}</li> ) }
+					</div>
+				</div>	
+				);
+         	}}
+			</Query>
 		);
-}
+	}
 }
 
 
